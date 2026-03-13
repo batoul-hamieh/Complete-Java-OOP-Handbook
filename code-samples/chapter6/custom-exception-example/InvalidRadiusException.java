20a15d831618578100485b733f55df55eeb45d48
Batoul Hamieh <batoull.hamieh@gmail.com>
2026-03-13 14:56:29 +0200

    Create InvalidRadiusException.java

---
+++ code-samples/chapter6/custom-exception-example/InvalidRadiusException.java
@@ -0,0 +1,5 @@
+public class InvalidRadiusException extends Exception {
+    public InvalidRadiusException(String message) {
+        super(message); // calling the constructor of Exception
+    }
+}
