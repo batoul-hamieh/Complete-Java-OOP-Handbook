01ccd31f24edc3cf00151b4c10dc6e1dbecaa176
Batoul Hamieh <batoull.hamieh@gmail.com>
2026-03-13 10:49:22 +0200

    Create Circle.java

---
+++ code-samples/chapter1/statics-example/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    double radius;
+    static int nbOfObjects = 0;
+
+    public Circle(double r) {
+        radius = r;
+        nbOfObjects++;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public static int getNbOfObjects() {
+        return nbOfObjects;
+    }
+}
