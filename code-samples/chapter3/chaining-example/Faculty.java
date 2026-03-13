851518cfa55209a9be8bae08af0e195e2175f37d
Batoul Hamieh <batoull.hamieh@gmail.com>
2026-03-13 11:22:27 +0200

    Create Faculty.java

---
+++ code-samples/chapter3/chaining-example/Faculty.java
@@ -0,0 +1,9 @@
+class Faculty extends Employee {
+    public static void main(String[] args) {
+        new Faculty();
+    }
+
+    public Faculty() {
+        System.out.println("Faculty's no-arg constructor is invoked");
+    }
+}
